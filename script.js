c8cf2362aefcd04371e1bd539d0daef869b38bbc
Brian Olson <bwolson31@gmail.com>
2024-02-05 18:48:01 -0600

    Added content for modal

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function (){
+    let modal = document.getElementById('myModal');
+    let closeButton = document.querySelector('.close');
+    modal.style.display = 'block';
+    closeButton.addEventListener('click', function () {
+        modal.style.display = 'none';
+    });
+    window.addEventListener('click', function(event) {
+        if (event.target === modal)
+{
+    modal.style.display = 'none';
+}    });
+});
